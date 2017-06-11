dcac6404be6093af1c7f2cfbca88114c358acea0
rudy <rudy_steiner@163.com>
2017-06-11 14:49:05 +0800

    [rudy] add test result txt

--- src/test/java/dbenchmark/db/H2MVStore.java
+++ src/test/java/dbenchmark/db/H2MVStore.java
@@ -8,8 +8,16 @@ import org.h2.mvstore.MVStore;
  * Created by Rudy Steiner on 2017/6/10.
  */
 public class H2MVStore {
-    private static MVStore mvStore= MVStore.open(Constants.H2_DB_FILE);
 
+   // private static MVStore mvStore= MVStore.open(Constants.H2_DB_FILE);
+    private static MVStore mvStore= new MVStore.Builder()
+                                 .fileName(Constants.H2_DB_FILE)
+                                 .cacheSize(24) //default 16
+//                               .autoCommitDisabled()
+//                               .autoCommitBufferSize(24) //default 1024
+//                               .readOnly()
+//                               .compressHigh()
+                                 .open();
     public static MVMap<String,String> getMVMap(String name){
       return   mvStore.openMap(name);
     }
