c626f8c7bd8634d2a34a0acd452b50951e92f4fb
Fan Xie <fxie1@andrew.cmu.edu>
2017-06-11 18:54:16 +0800

    add PageCacheHelperTest

---
+++ src/test/java/com/alibaba/middleware/race/sync/PageCacheHelperTest.java
@@ -0,0 +1,13 @@
+package com.alibaba.middleware.race.sync;
+
+import org.junit.Test;
+
+/**
+ * Created by xiefan on 6/11/17.
+ */
+public class PageCacheHelperTest {
+	@Test
+	public void test() throws Exception {
+		new PageCacheHelper().run();
+	}
+}
